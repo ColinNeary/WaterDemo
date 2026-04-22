25f9228ebce75d547bdc72e61d245141c942e384
ColinNeary <colinneary2749@gmail.com>
2026-04-22 14:48:48 -0500

    Initial ripple effect + initial game scene

---
+++ Scenes/main.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var cr = $Simulation/ColorRect
+	var water = $Water
+	var sim_tex = $Simulation.get_texture()
+	var col_tex = $Collision.get_texture()
+
+	water.mesh.surface_get_material(0).set_shader_parameter('simulation', sim_tex)
